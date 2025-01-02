3dc477e37143c768686d3124ac91b2d788df95b2
Christoph Wagner <christoph.c.wagner@bmw.de>
2025-01-02 09:19:49 +0100

    sicherung a

--- lua/linter/plugins/nonels.lua
+++ lua/linter/plugins/nonels.lua
@@ -5,7 +5,8 @@ return {
       local null_ls = require 'null-ls'
       null_ls.setup {
         sources = {
-          null_ls.builtins.diagnostics.flake8,
+          -- null_ls.builtins.diagnostics.flake8,
+          null_ls.builtins.diagnostics.flake8.with { extra_args = { '--max-line-length', '120' } },
           null_ls.builtins.diagnostics.markdownlint,
           null_ls.builtins.diagnostics.jsonlint,
           -- null_ls.builtins.completion.spell,
